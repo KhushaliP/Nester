81e4a75ee268c1beca6ae8c0334236f0c1fa2b40
KhushaliP <59303223+KhushaliP@users.noreply.github.com>
2020-09-13 10:10:04 +0530

    Update setup.py

--- nester/setup.py
+++ nester/setup.py
@@ -3,9 +3,9 @@ from distutils.core import setup
 
 setup(
 	 name = 'nester',
-	 version = '1.0.0',
+	 version = '1.1.0',
 	 py_modules = ['nester'],
-	 author = 'kppython',
+	 author = 'Khushali',
 	 author_email = 'khushalibpatel@gmail.com',
 	 url = 'http://www.headfirstlabs.com',
 	 description = 'A simple printer of nested lists',
